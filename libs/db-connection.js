eab34ff4f67bdc88108a13b53db4a009145548b9
Felipe Alvarez <felipeag06@gmail.com>
2019-10-14 02:59:48 -0500

    Mlab database config added

--- libs/db-connection.js
+++ libs/db-connection.js
@@ -4,7 +4,11 @@ let db;
 
 module.exports = function Connection() {
   if (!db) {
-    db = mongoose.createConnection('mongodb://localhost/gameOfDrones');
+    // db = mongoose.createConnection('mongodb://localhost/gameOfDrones');
+
+    db = mongoose.createConnection(
+      'mongodb://falvarez:testingpass1234@ds135128.mlab.com:35128/game-of-drones'
+    );
   }
   return db;
 };
